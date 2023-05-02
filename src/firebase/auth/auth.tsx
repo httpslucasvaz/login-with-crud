21c4505ffeada2c2f56de31a2f8d7c81e11b8018
Lucas Vaz <httpslucasvaz@gmail.com>
2023-05-01 22:31:50 -0300

    feat: configuração do firebase

---
+++ src/firebase/auth/auth.tsx
@@ -0,0 +1,25 @@
+import firebase_app from "../config";
+import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
+
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+    prompt: 'select_account'
+})
+
+const auth = getAuth(firebase_app);
+
+export const login = async () => {
+
+    await signInWithPopup(auth, provider)
+        .then((result) => {
+            console.log(result)
+        }).catch((err) => console.log(err))
+}
+
+export const logout = () => {
+    signOut(auth).then(() => {
+        console.log('deslogado')
+    }).catch((err) => console.log(err))
+};
