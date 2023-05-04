fafad5dd3b7846b64827d3cfb6a7d5d0939ebc3c
Lucas Vaz <httpslucasvaz@gmail.com>
2023-05-03 22:58:17 -0300

    feat: verificação de login

--- src/firebase/auth/auth.tsx
+++ src/firebase/auth/auth.tsx
@@ -1,5 +1,7 @@
+import { useRouter } from "next/navigation";
 import firebase_app from "../config";
 import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
+import { APP_ROUTES } from "@/app/constants/app-routes";
 
 
 const provider = new GoogleAuthProvider();
@@ -8,14 +10,18 @@ provider.setCustomParameters({
     prompt: 'select_account'
 })
 
+
 export const auth = getAuth(firebase_app);
 
 export const login = async () => {
+    const route = useRouter()
 
+    return (
     await signInWithPopup(auth, provider)
-        .then((result) => {
-            console.log(result)
+        .then(() => {
+            route.push(APP_ROUTES.private.home)
         }).catch((err) => console.log(err))
+        )
 }
 
 export const logout = () => {
