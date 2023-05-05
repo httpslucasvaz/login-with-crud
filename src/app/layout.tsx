617aeaeeaa86257ba5b3c733cbc3186f018d6b31
Lucas Vaz <httpslucasvaz@gmail.com>
2023-05-04 21:45:19 -0300

    feat: add verificação para rota privada

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,14 @@
 'use client'
 
+
 import { auth } from '@/firebase/auth/auth'
+import { onAuthStateChanged } from 'firebase/auth'
 import { Poppins } from 'next/font/google'
+import { usePathname, useRouter } from 'next/navigation'
+import { APP_ROUTES } from './constants/app-routes'
+import { useEffect } from 'react'
 import PrivateRoute from './components/privateRoute'
-import { checkIsPublicRoute } from '@/functions/check-is-public-route'
-import { usePathname } from 'next/navigation'
+import { checkIsPublicRoute } from './functions/check-is-public-route'
 
 const poppins = Poppins({
     weight: '300',
@@ -16,16 +20,16 @@ export default function RootLayout({
 }: {
     children: React.ReactNode
 }) {
-    const pathname = usePathname()
+    const  pathname  = usePathname()
     const isPublicPage = checkIsPublicRoute(pathname!)
-    console.log(isPublicPage)
-
+    
     return (
         <html lang="pt-BR">
             <body className={poppins.className}>
-
-                {isPublicPage && children}
-                {!isPublicPage && <PrivateRoute> {children} </PrivateRoute>}
+                <>
+                    {isPublicPage && children}
+                    {!isPublicPage && <PrivateRoute>{children}</PrivateRoute>}
+                </>
 
             </body>
         </html>
