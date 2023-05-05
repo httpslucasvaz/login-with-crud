d349a052268ae8ef6f89926c2110be50e41f4e55
Lucas Vaz <httpslucasvaz@gmail.com>
2023-05-04 21:44:28 -0300

    feat: configuração para autenticação

--- src/firebase/auth/auth.tsx
+++ src/firebase/auth/auth.tsx
@@ -1,7 +1,6 @@
-import { useRouter } from "next/navigation";
+
 import firebase_app from "../config";
 import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
-import { APP_ROUTES } from "@/app/constants/app-routes";
 
 
 const provider = new GoogleAuthProvider();
@@ -14,14 +13,14 @@ provider.setCustomParameters({
 export const auth = getAuth(firebase_app);
 
 export const login = async () => {
-    const route = useRouter()
+
 
     return (
-    await signInWithPopup(auth, provider)
-        .then(() => {
-            route.push(APP_ROUTES.private.home)
-        }).catch((err) => console.log(err))
-        )
+        await signInWithPopup(auth, provider)
+            .then(() => {
+
+            }).catch((err) => console.log(err))
+    )
 }
 
 export const logout = () => {
