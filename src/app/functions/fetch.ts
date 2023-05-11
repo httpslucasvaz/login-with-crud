a6eeaed4c33f62e297a0c2c45eb9fa4d50391913
Lucas Vaz <httpslucasvaz@gmail.com>
2023-05-10 22:06:34 -0300

    deletado

--- src/app/functions/fetch.ts
+++
@@ -1,6 +0,0 @@
-export async function fetchWrapper(url: string) {
-    const data = await fetch(`${process.env.NEXT_BASE_URL}?api_key=${process.env.NEXT_API_KEY}&language=pt-BR&page=1&include_adult=false&query=${url}`);
-    const result = await data.json();
-
-    return result
-}
