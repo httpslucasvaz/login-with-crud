5eb0c2ab3a4cb55b2a6cc720ba8754f450f1e76a
Lucas Vaz <httpslucasvaz@gmail.com>
2023-05-17 23:11:10 -0300

    feat: verificação se pesquisa está sendo feita para exibição de componente

---
+++ src/app/components/hero/index.tsx
@@ -0,0 +1,21 @@
+'use client'
+import { Header } from "./header";
+import { Movies } from "./movies";
+import { Summary } from "./summary";
+import { InputSearch } from "./inputSearch";
+import { useContext } from "react";
+import { MyContext } from "@/context/isSearchOn";
+
+
+
+export function Hero() {
+    const { isSearchOn } = useContext(MyContext)
+    return (
+        <>
+            <Header />
+            <Summary />
+            <InputSearch />
+            {!isSearchOn && <Movies /> }
+        </>
+    )
+}
