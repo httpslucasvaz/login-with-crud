19adbfc3ba983175c8be5494bd67b3e597dcefc8
Lucas Vaz <httpslucasvaz@gmail.com>
2023-05-18 23:48:20 -0300

    feat: add configuração para firestore

---
+++ src/firebase/db/firestore.ts
@@ -0,0 +1,6 @@
+import { collection, getFirestore } from "firebase/firestore";
+import firebase_app from "../config";
+
+export const db = getFirestore(firebase_app)
+
+export const userCollection = collection(db, 'users')
